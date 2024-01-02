3dde7399437ce54599169c75ec31ffb73b760ac7
N3v1 <n3v1010@gmail.com>
2024-01-02 11:16:38 +0100

    [Core]: Add custom fonts

---
+++ Sources/ThemeManager/Fonts/Font.swift
@@ -0,0 +1,23 @@
+//
+//  Font.swift
+//
+//
+//  Created by Nevio Hirani on 02.01.24.
+//
+
+import Foundation
+import SwiftUI
+
+public struct Fonts {
+    fileprivate static func registerFont(bundle: Bundle, fontName: String, fontExtension: String) {
+        guard let fontURL = bundle.url(forResource: fontName, withExtension: fontExtension),
+              let fontDataProvider = CGDataProvider(url: fontURL as CFURL),
+              let font = CGFont(fontDataProvider) else {
+            fatalError("Couldn't create font from filename: \(fontName) with extension \(fontExtension)")
+        }
+        
+        var error: Unmanaged<CFError>?
+        
+        CTFontManagerRegisterGraphicsFont(font, &error)
+    }
+}
